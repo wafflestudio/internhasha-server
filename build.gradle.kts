8d68c3404f64a26dbb1dc81522eb5cdeb78a6d44
endermaru <endermaru@snu.ac.kr>
2024-12-25 16:56:16 +0900

    modify ktlint options & apply ktlintFormat

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.jpa") version "2.0.20"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
 }
 
-group = "com.ToyProject"
+group = "com.waffletoy"
 version = "0.0.1-SNAPSHOT"
 
 java {
@@ -17,6 +20,7 @@ java {
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
@@ -36,11 +40,20 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+
+    implementation("com.mysql:mysql-connector-j:8.2.0")
 }
 
 kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
     }
 }
 
--- src/main/kotlin/com/waffletoy/team1server/pretotype/persistence/PretotypeRepository.kt
+++ src/main/kotlin/com/waffletoy/team1server/pretotype/persistence/PretotypeRepository.kt
@@ -2,6 +2,6 @@ package com.waffletoy.team1server.pretotype.persistence
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PretotypeRepository: JpaRepository<PretotypeEntity, String> {
+interface PretotypeRepository : JpaRepository<PretotypeEntity, String> {
     fun findByEmail(email: String): PretotypeEntity?
 }
