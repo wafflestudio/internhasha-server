8224f1188e825692b03caf26aa6e3a31c54c5e54
endermaru <endermaru@snu.ac.kr>
2024-12-27 20:30:03 +0900

    add Webconfig(cors)

---
+++ src/main/kotlin/com/waffletoy/team1server/WebConfig.kt
@@ -0,0 +1,16 @@
+package com.waffletoy.team1server
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+            .allowedOrigins("https://survey-josha.site") // 허용할 도메인
+            .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+            .allowedHeaders("*") // 허용할 헤더
+            .allowCredentials(true) // 인증 정보(쿠키 등) 허용
+    }
+}
