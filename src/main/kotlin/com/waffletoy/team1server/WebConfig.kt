46a8c4fe622742a7270f0e6f89fd42c6d9110af1
endermaru <127807229+endermaru@users.noreply.github.com>
2024-12-27 21:41:01 +0900

    Cors (#19)
    * add Webconfig(cors)
    
    * add origin
    
    * fix cors application.yml
    
    * change deploy.yml

--- src/main/kotlin/com/waffletoy/team1server/WebConfig.kt
+++ src/main/kotlin/com/waffletoy/team1server/WebConfig.kt
@@ -8,7 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-            .allowedOrigins("https://survey-josha.site", "https://www.survey-josha.site") // 허용할 도메인
+            .allowedOrigins("https://survey-josha.site",
+                "https://www.survey-josha.site",
+                "https://d3lb937auepw3n.cloudfront.net",
+                "https://d1vq7k80ej9gk7.cloudfront.net",
+                ) // 허용할 도메인
             .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
             .allowedHeaders("*") // 허용할 헤더
             .allowCredentials(true) // 인증 정보(쿠키 등) 허용
