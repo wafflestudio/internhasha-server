919edfc797944cd841db9b439fec45d650073eb3
endermaru <127807229+endermaru@users.noreply.github.com>
2024-12-28 21:52:29 +0900

    Update Application.kt
    Swagger url을 https로 변경

--- src/main/kotlin/com/waffletoy/team1server/Application.kt
+++ src/main/kotlin/com/waffletoy/team1server/Application.kt
@@ -1,8 +1,18 @@
 package com.waffletoy.team1server
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(
+    servers = [
+        Server(
+            url = "/",
+            description = "Default Server URL",
+        ),
+    ],
+)
 @SpringBootApplication
 class Application
 
