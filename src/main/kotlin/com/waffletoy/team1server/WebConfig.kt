96ec6898aff68c8ada05a6116bfcf22fbf9ebb48
endermaru <127807229+endermaru@users.noreply.github.com>
2024-12-31 19:11:24 +0900

    Update WebConfig.kt

--- src/main/kotlin/com/waffletoy/team1server/WebConfig.kt
+++ src/main/kotlin/com/waffletoy/team1server/WebConfig.kt
@@ -1,12 +1,12 @@
 package com.waffletoy.team1server
 
-// import org.springframework.context.annotation.Configuration
-// import org.springframework.web.servlet.config.annotation.CorsRegistry
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-// @Configuration
-// class WebConfig : WebMvcConfigurer {
-//     override fun addCorsMappings(registry: CorsRegistry) {
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
 //         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
 //             .allowedOrigins(
 //                 "https://survey-josha.site",
@@ -18,5 +18,5 @@ package com.waffletoy.team1server
 //             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
 //             .allowedHeaders("*") // 허용할 헤더
 //             .allowCredentials(true) // 인증 정보(쿠키 등) 허용
-//     }
-// }
+    }
+}
