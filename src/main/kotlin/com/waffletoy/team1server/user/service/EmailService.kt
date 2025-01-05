ea056dc4eb96c61a725f7a5a378cce1f4ced616c
endermaru <endermaru@snu.ac.kr>
2025-01-05 21:57:27 +0900

    emailService와 그에 따른 redisTokenService 수정

--- src/main/kotlin/com/waffletoy/team1server/user/service/EmailService.kt
+++ src/main/kotlin/com/waffletoy/team1server/user/service/EmailService.kt
@@ -2,6 +2,7 @@ package com.waffletoy.team1server.user.service
 
 import com.waffletoy.team1server.user.EmailServiceException
 import com.waffletoy.team1server.user.persistence.UserEntity
+import com.waffletoy.team1server.user.persistence.UserRepository
 import org.mindrot.jbcrypt.BCrypt
 import org.springframework.http.HttpStatus
 import org.springframework.mail.SimpleMailMessage
@@ -13,54 +14,34 @@ import java.util.*
 @Service
 class EmailService(
     private val mailSender: JavaMailSender,
+    private val userRepository: UserRepository,
     private val redisTokenService: RedisTokenService,
 ) {
-    fun verifyToken(
-        userId: String,
-        token: String,
-    ): String {
-        // Redis에서 암호화된 이메일 인증 코드 가져오기
-        val encryptedToken =
-            redisTokenService.getEmailTokenByUserId(userId)
-                ?: throw EmailServiceException(
-                    "Invalid or expired email token",
-                    HttpStatus.BAD_REQUEST,
-                )
-
-        // 입력된 인증 코드와 Redis에 저장된 암호화된 코드 비교
-        return try {
-            if (!BCrypt.checkpw(token, encryptedToken)) {
-                throw EmailServiceException(
-                    "Invalid email token",
-                    HttpStatus.BAD_REQUEST,
-                )
-            }
-
-            // 인증 성공 시 사용자 ID 반환
-            userId
-        } finally {
-            // 토큰 삭제 (성공/실패 관계없이 무조건 삭제)
-//            redisTokenService.deleteEmailTokenByUserId(userId)
-        }
-    }
 
     @Async
-    fun sendEmailVerification(
-        user: UserEntity,
-        email: String,
+    fun sendCode(
+        snuMail: String,
     ) {
+        // 이미 등록된 메일인지 확인
+        if (userRepository.findBySnuMail(snuMail) != null) {
+            throw EmailServiceException(
+                "동일한 스누메일로 등록된 계정이 존재합니다.",
+                HttpStatus.CONFLICT,
+            )
+        }
+
         // 이메일 인증 토큰 생성
         val emailCode = (100000..999999).random().toString()
 
         val encryptedEmailCode = BCrypt.hashpw(emailCode, BCrypt.gensalt())
 
         // Redis 에 Email Token 저장
-        redisTokenService.saveEmailToken(user.id, encryptedEmailCode)
+        redisTokenService.saveEmailCode(snuMail, encryptedEmailCode)
 
         // 이메일 발송
         try {
             val message = SimpleMailMessage()
-            message.setTo(email)
+            message.setTo(snuMail)
             message.subject = "이메일 인증 요청"
             message.text = "이메일 인증 번호: $emailCode"
             mailSender.send(message)
@@ -69,6 +50,30 @@ class EmailService(
                 "Sending Email Failure",
                 HttpStatus.INTERNAL_SERVER_ERROR,
             )
+        }
+    }
+
+    fun verifyToken(
+        snuMail: String,
+        code: String,
+    ) {
+        // Redis에서 암호화된 이메일 인증 코드 가져오기
+        val encryptedCode =
+            redisTokenService.getEmailCode(snuMail)
+                ?: throw EmailServiceException(
+                    "이메일로 전달된 코드가 유효하지 않습니다.",
+                    HttpStatus.FORBIDDEN,
+                )
+
+        // 입력된 인증 코드와 Redis에 저장된 암호화된 코드 비교
+        if (!BCrypt.checkpw(code, encryptedCode)) {
+            throw EmailServiceException(
+                "인증 코드와 입력 코드가 일치하지 않습니다.",
+                HttpStatus.BAD_REQUEST,
+            )
+        }
+        else {
+            redisTokenService.deleteEmailCode(snuMail)
         }
     }
 }
