e154580ccd7b93f069698106f56d7f984addb188
Jonghyun Lee <lukeqwaszx@gmail.com>
2025-01-05 20:10:11 +0900

    fix: delete older code

--- src/main/kotlin/com/waffletoy/team1server/pretotype/PretotypeEmailConflictException.kt
+++
@@ -1,6 +0,0 @@
-package com.waffletoy.team1server.pretotype
-
-import com.waffletoy.team1server.DomainException
-import org.springframework.http.HttpStatus
-
-class PretotypeEmailConflictException : DomainException(0, HttpStatus.CONFLICT, "Email already exists")
