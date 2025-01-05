acaec18be1a22dc77f591881b81aaeeb8f5d0ebb
Jonghyun Lee <lukeqwaszx@gmail.com>
2025-01-05 20:10:11 +0900

    fix: delete older code

--- src/main/kotlin/com/waffletoy/team1server/pretotype/persistence/PretotypeRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.waffletoy.team1server.pretotype.persistence
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-// ID는 Long
-interface PretotypeRepository : JpaRepository<PretotypeEntity, Long> {
-    fun findByEmail(email: String): PretotypeEntity?
-}
