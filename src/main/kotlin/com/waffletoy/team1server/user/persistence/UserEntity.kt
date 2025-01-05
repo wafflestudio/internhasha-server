063858abbea2bc6bc270e25147c13caa993f243d
endermaru <endermaru@snu.ac.kr>
2025-01-05 20:42:03 +0900

    userEntity를 erd에 맞게 수정

--- src/main/kotlin/com/waffletoy/team1server/user/persistence/UserEntity.kt
+++ src/main/kotlin/com/waffletoy/team1server/user/persistence/UserEntity.kt
@@ -21,30 +21,20 @@ open class UserEntity(
     @Column(name = "snu_mail", nullable = false, unique = true)
     val snuMail: String,
     // 유저 표시 이름
-    @Column(name = "nickname", nullable = false)
-    var nickname: String,
-    // 계정 상태(활성화, 비활성화)
-    @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false)
-    var status: UserStatus,
-    // 로그인 방법 - local or google
-    @Enumerated(EnumType.STRING)
-    @Column(name = "auth_provider", nullable = false)
-    var authProvider: AuthProvider,
-    // 로그인 아이디
-    @Column(name = "login_id", nullable = true, unique = true)
-    val loginId: String? = null,
+    @Column(name = "user_name", nullable = false)
+    var userName: String,
+    // 로컬 아이디
+    @Column(name = "local_id", nullable = true, unique = true)
+    val localId: String? = null,
     // 해시된 비밀번호 저장
     @Column(name = "password", nullable = true)
     var password: String? = null,
-    // 추가적인 소셜 로그인 정보
     // 구글 ID
     @Column(name = "google_id", nullable = true, unique = true)
     val googleId: String? = null,
-    // 구글 이메일
-    @Column(name = "google_email", nullable = true, unique = true)
-    val googleEmail: String? = null,
-    // 추가적인 개인 정보
+    // 전화번호
+    @Column(name = "phone_number", nullable = true)
+    val phoneNumber: String? = null,
     // 생성, 수정 시간
     @CreatedDate
     @Column(nullable = false, updatable = false)
@@ -52,9 +42,4 @@ open class UserEntity(
     @LastModifiedDate
     @Column(nullable = false)
     var updatedAt: Instant = Instant.now(),
-    // 작성한 포스트
-    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
-    val authoredPosts: Set<PostEntity> = emptySet(),
-    //    @OneToMany(mappedBy = "participant", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
-    //    val participations: Set<ParticipationEntity> = emptySet(), // 이거 어떻게 쓸지는 고민을 해봐야겠어요
 )
