4b952047363e3675ea6af50c8ee1a7412667a15b
endermaru <127807229+endermaru@users.noreply.github.com>
2025-01-07 16:44:17 +0900

    소셜 로그인 버그 수정(User, Admin Entity 수정) (#42)
    * User, Admin 엔티티 생성자 수정
    
    * remove debug

--- src/main/kotlin/com/waffletoy/team1server/account/persistence/AdminEntity.kt
+++ src/main/kotlin/com/waffletoy/team1server/account/persistence/AdminEntity.kt
@@ -7,9 +7,9 @@ import jakarta.persistence.*
 class AdminEntity(
     @Column(name = "profile_image_link", length = 255)
     var profileImageLink: String? = null,
-    override var username: String,
-    override var password: String? = null,
-    override var localId: String? = null,
+    username: String,
+    password: String? = null,
+    localId: String? = null,
 //    @OneToMany(mappedBy = "admin", cascade = [CascadeType.ALL], orphanRemoval = true)
 //    val posts: List<PostEntity> = mutableListOf()
 ) : AccountEntity(
