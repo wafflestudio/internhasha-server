a42ff68520e50f1185fb92c3203055525d500083
endermaru <127807229+endermaru@users.noreply.github.com>
2025-01-08 19:58:21 +0900

    버그 수정 (#45)

--- src/main/kotlin/com/waffletoy/team1server/account/persistence/AdminEntity.kt
+++ src/main/kotlin/com/waffletoy/team1server/account/persistence/AdminEntity.kt
@@ -1,6 +1,5 @@
 package com.waffletoy.team1server.account.persistence
 
-import com.waffletoy.team1server.post.persistence.PostEntity
 import jakarta.persistence.*
 
 @Entity(name = "admins")
@@ -8,7 +7,6 @@ import jakarta.persistence.*
 class AdminEntity(
     @Column(name = "profile_image_link", length = 255)
     var profileImageLink: String? = null,
-
     username: String,
     password: String? = null,
     localId: String? = null,
