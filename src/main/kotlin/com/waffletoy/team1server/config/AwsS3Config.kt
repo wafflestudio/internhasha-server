f732f3ebef8b12400df316577d7d9769db06000d
endermaru <127807229+endermaru@users.noreply.github.com>
2025-01-19 19:57:48 +0900

    admin package for presigend url (#68)

---
+++ src/main/kotlin/com/waffletoy/team1server/config/AwsS3Config.kt
@@ -0,0 +1,34 @@
+package com.waffletoy.team1server.config
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AwsS3Config {
+    @Value("\${amazon.aws.accessKey}")
+    private val accessKeyId: String? = null
+
+    @Value("\${amazon.aws.secretKey}")
+    private val accessKeySecret: String? = null
+
+    @Value("\${amazon.aws.region}")
+    private val s3RegionName: String? = null
+
+    @get:Bean
+    val amazonS3Client: AmazonS3
+        get() {
+            val basicAWSCredentials: BasicAWSCredentials = BasicAWSCredentials(accessKeyId, accessKeySecret)
+
+            // Get Amazon S3 client and return the s3 client object
+            return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(AWSStaticCredentialsProvider(basicAWSCredentials))
+                .withRegion(s3RegionName)
+                .build()
+        }
+}
