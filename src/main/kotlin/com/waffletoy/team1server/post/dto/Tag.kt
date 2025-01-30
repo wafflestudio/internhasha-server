d7b1d7b5b8cf0adc7e2d54e96ea3a5bcc8fe982b
Jonghyun Lee <lukeqwaszx@gmail.com>
2025-01-30 22:57:19 +0900

    feat: fromVo introduced

---
+++ src/main/kotlin/com/waffletoy/team1server/post/dto/Tag.kt
@@ -0,0 +1,11 @@
+package com.waffletoy.team1server.post.dto
+
+data class Tag (
+    val tag: String
+) {
+    companion object {
+        fun fromVo(tagVo: TagVo): Tag {
+            return Tag(tag = tagVo.tag)
+        }
+    }
+}
