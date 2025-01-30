170507d207e546c47cd4f986fc72151bdde14e60
Jonghyun Lee <lukeqwaszx@gmail.com>
2025-01-30 23:08:24 +0900

    feat: fix bugs

--- src/main/kotlin/com/waffletoy/team1server/post/persistence/CompanyRepository.kt
+++ src/main/kotlin/com/waffletoy/team1server/post/persistence/CompanyRepository.kt
@@ -1,19 +1,10 @@
 package com.waffletoy.team1server.post.persistence
 
-import com.waffletoy.team1server.user.persistence.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CompanyRepository : JpaRepository<CompanyEntity, String> {
-    /**
-     * Finds a company by its admin user.
-     *
-     * @param admin The admin user.
-     * @return The associated CompanyEntity or null if not found.
-     */
-    fun findByCurator(Curator: UserEntity): CompanyEntity?
-
     /**
      * Checks if a company with the given email exists.
      *
