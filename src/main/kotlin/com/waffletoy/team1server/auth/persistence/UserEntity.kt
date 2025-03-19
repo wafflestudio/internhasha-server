3b3df502b28eb7721b825cf8c7f3e774e47c0a92
Jaemin Kim <odumag99@naver.com>
2025-03-19 17:32:24 +0900

    UserEntity.profileImageLink 복구 및 Deprecated 어노테이션 추가

--- src/main/kotlin/com/waffletoy/team1server/auth/persistence/UserEntity.kt
+++ src/main/kotlin/com/waffletoy/team1server/auth/persistence/UserEntity.kt
@@ -35,7 +35,10 @@ class UserEntity(
     @Column(nullable = false)
     val userRole: UserRole,
     @OneToOne(mappedBy = "user", cascade = [CascadeType.ALL], fetch = FetchType.LAZY, optional = true)
-    val company: CompanyEntity? = null
+    val company: CompanyEntity? = null,
+    @Deprecated("This field will be moved to ApplicantEntity, CompanyEntity respectively.")
+    @Column(name = "profile_image_link", nullable = true, length = 2048)
+    val profileImageLink: String? = null
     // APPLICATNT specific field
 //    @Column(name = "phone_number", nullable = true)
 //    var phoneNumber: String? = null,,
