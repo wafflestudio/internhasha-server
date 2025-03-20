7ec2a8491589cb6745aa12257b0fdaa189b2e517
Jaemin Kim <odumag99@naver.com>
2025-03-20 21:13:43 +0900

    Link 에러 메시지 수정

--- src/main/kotlin/com/waffletoy/team1server/applicant/dto/Link.kt
+++ src/main/kotlin/com/waffletoy/team1server/applicant/dto/Link.kt
@@ -6,7 +6,7 @@ import org.hibernate.validator.constraints.URL
 
 data class Link(
     @field:NotBlank
-    @field:URL
+    @field:URL(message = "link가 URL의 형식이 아닙니다.")
     @field:Size(message = "link가 최대 글자수를 초과했습니다.", max = 2047)
     val link: String,
     @field:NotBlank
