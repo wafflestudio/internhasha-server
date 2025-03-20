14fef772ba229c4158645c32ca38dc74081e6ceb
Jaemin Kim <odumag99@naver.com>
2025-03-20 21:08:54 +0900

    Applicant 패키지 및 ApplicantEntity 생성

---
+++ src/main/kotlin/com/waffletoy/team1server/applicant/dto/Link.kt
@@ -0,0 +1,16 @@
+package com.waffletoy.team1server.applicant.dto
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import org.hibernate.validator.constraints.URL
+
+data class Link (
+    @field:NotBlank
+    @field:URL
+    @field:Size(message = "link가 최대 글자수를 초과했습니다.", max = 2047)
+    val link: String,
+
+    @field:NotBlank
+    @field:Size(message = "description이 최대 글자수를 초과했습니다.", max= 30)
+    val description: String
+)
