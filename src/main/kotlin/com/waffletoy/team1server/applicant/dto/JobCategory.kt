49f05148e18e9c9e723424406923a228ee590158
Jaemin Kim <odumag99@naver.com>
2025-03-21 21:56:02 +0900

    JobCategory Enum 클래스 정의

---
+++ src/main/kotlin/com/waffletoy/team1server/applicant/dto/JobCategory.kt
@@ -0,0 +1,12 @@
+package com.waffletoy.team1server.applicant.dto
+
+enum class JobCategory {
+    FRONT,
+    APP,
+    BACKEND,
+    DATA,
+    OTHERS,
+    DESIGN,
+    PLANNER,
+    MARKETING,
+}
