98c6f33dbd0cf3a0d073d601483c1fa5a0569637
Jaemin Kim <odumag99@naver.com>
2025-03-21 12:55:59 +0900

    Applicant Controller 추가

---
+++ src/main/kotlin/com/waffletoy/team1server/applicant/controller/ApplicantController.kt
@@ -0,0 +1,22 @@
+package com.waffletoy.team1server.applicant.controller
+
+import com.waffletoy.team1server.auth.AuthUser
+import com.waffletoy.team1server.auth.dto.User
+import io.swagger.v3.oas.annotations.Parameter
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/applicant")
+class ApplicantController(
+
+) {
+    @GetMapping("/me")
+    fun getMe(
+        @Parameter(hidden = true) @AuthUser user: User,
+    ) : ResponseEntity<User> {
+        return ResponseEntity.ok(user)
+    }
+}
