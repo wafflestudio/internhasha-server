ea6b4b80f99d601e2dde235e3d24460910605659
Jaemin Kim <odumag99@naver.com>
2025-03-21 14:17:59 +0900

    ApplicationService.getApplicant 완성

--- src/main/kotlin/com/waffletoy/team1server/applicant/service/ApplicantService.kt
+++ src/main/kotlin/com/waffletoy/team1server/applicant/service/ApplicantService.kt
@@ -1,5 +1,7 @@
 package com.waffletoy.team1server.applicant.service
 
+import com.waffletoy.team1server.applicant.ApplicantNotFoundException
+import com.waffletoy.team1server.applicant.ApplicantUserForbiddenException
 import com.waffletoy.team1server.applicant.persistence.ApplicantEntity
 import com.waffletoy.team1server.applicant.persistence.ApplicantRepository
 import com.waffletoy.team1server.auth.UserRole
@@ -15,7 +17,9 @@ class ApplicantService(
     ) : ApplicantEntity {
         // User가 Applicant가 맞는지 확인
         if ( user.userRole != UserRole.APPLICANT ) {
-            TODO("NotApplicantError 반환")
+            throw ApplicantUserForbiddenException(
+                details = mapOf("userId" to user.id, "userRole" to user.userRole),
+            )
         }
 
         // User의 Applicant 정보 받기
@@ -23,7 +27,7 @@ class ApplicantService(
 
         // applicant null이면 정보 없음 반환
         if (applicantEntity == null) {
-            TODO("ApplicantNotFoundError 반환")
+            throw ApplicantNotFoundException()
         }
 
         // 정보 반환
