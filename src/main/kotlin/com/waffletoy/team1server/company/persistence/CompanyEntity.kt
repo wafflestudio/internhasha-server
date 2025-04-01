7194d4051e04fc6fbd28f76800164a09a9b39953
Jaemin Kim <odumag99@naver.com>
2025-04-01 18:23:21 +0900

    CompanyEntity.vcName, .vcRec nullable 처리

--- src/main/kotlin/com/waffletoy/team1server/company/persistence/CompanyEntity.kt
+++ src/main/kotlin/com/waffletoy/team1server/company/persistence/CompanyEntity.kt
@@ -38,11 +38,11 @@ class CompanyEntity(
     @Column(name = "COMPANY_INFO_PDF_LINK", length = 2048, nullable = true)
     open var companyInfoPDFLink: String? = null,
     @Column(name = "LANDING_PAGE_LINK", length = 2048, nullable = true)
-    open var landingPageLink: String,
+    open var landingPageLink: String?,
     @Column(name = "VC_NAME", nullable = true)
-    open var vcName: String,
+    open var vcName: String? = null,
     @Column(name = "VC_REC", nullable = true)
-    open var vcRec: String,
+    open var vcRec: String? = null,
     @CreatedDate
     @Column(name = "CREATED_AT", nullable = false, updatable = false)
     open var createdAt: LocalDateTime = LocalDateTime.now(),
