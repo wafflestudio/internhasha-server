a0ca50687d44fff63e1e4ad93311b845d4cb1e0b
Gwangseop Lim <127807229+endermaru@users.noreply.github.com>
2025-04-14 17:25:14 +0900

    set spring profile, modified environments (#192)

--- src/test/kotlin/com/waffletoy/team1server/ApplicationTests.kt
+++ src/test/kotlin/com/waffletoy/team1server/ApplicationTests.kt
@@ -2,7 +2,9 @@ package com.waffletoy.team1server
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("local")
 @SpringBootTest
 class ApplicationTests {
     @Test
