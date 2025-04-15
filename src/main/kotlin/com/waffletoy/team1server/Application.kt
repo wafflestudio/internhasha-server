fd0e2ba5431bd7de318b15f20302d8de611173a4
endermaru <endermaru@snu.ac.kr>
2025-04-16 03:38:14 +0900

    applied Spring Actuator

--- src/main/kotlin/com/waffletoy/team1server/Application.kt
+++ src/main/kotlin/com/waffletoy/team1server/Application.kt
@@ -1,7 +1,8 @@
 package com.waffletoy.team1server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import java.util.*
 
@@ -11,5 +12,8 @@ class Application
 
 fun main(args: Array<String>) {
     TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
-    runApplication<Application>(*args)
+//    runApplication<Application>(*args)
+    SpringApplicationBuilder(Application::class.java)
+        .applicationStartup(BufferingApplicationStartup(2048))
+        .run(*args)
 }
