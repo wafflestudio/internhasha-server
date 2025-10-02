9e79b5900e0e3ac10747fe859450bb6b83233e2e
endermaru <endermaru@snu.ac.kr>
2025-10-03 00:20:16 +0900

    CORS 해제 설정 적용

--- src/main/kotlin/com/wafflestudio/internhasha/config/WebConfig.kt
+++ src/main/kotlin/com/wafflestudio/internhasha/config/WebConfig.kt
@@ -4,6 +4,7 @@ import com.wafflestudio.internhasha.auth.UserArgumentResolver
 import com.wafflestudio.internhasha.auth.UserOrNullArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -14,5 +15,14 @@ class WebConfig(
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(userArgumentResolver)
         resolvers.add(userOrNullArgumentResolver)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(false)
+            .maxAge(3600)
     }
 }
