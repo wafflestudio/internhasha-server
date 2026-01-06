2ab02c07ea775769255103e12bbcd01f27b68f7e
endermaru <endermaru@snu.ac.kr>
2026-01-06 22:01:18 +0900

    Fix: RedisConfig에 database 환경변수 적용

--- src/main/kotlin/com/wafflestudio/internhasha/config/RedisConfig.kt
+++ src/main/kotlin/com/wafflestudio/internhasha/config/RedisConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
@@ -12,10 +13,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 class RedisConfig(
     @Value("\${spring.data.redis.host}") private val redisHost: String,
     @Value("\${spring.data.redis.port}") private val redisPort: Int,
+    @Value("\${spring.data.redis.database:0}") private val redisDatabase: Int,
 ) {
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory(redisHost, redisPort)
+        val config = RedisStandaloneConfiguration(redisHost, redisPort)
+        config.database = redisDatabase
+        return LettuceConnectionFactory(config)
     }
 
     @Bean
